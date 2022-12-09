e19fa04ffd9ece58c511ffb68be7328ee938b98b
Agustina <agustinasoledad2608@gmail.com>
2022-12-08 21:02:41 -0300

    Task 0. Updating

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A method that determines if all the boxes can be opened. """
+
+def canUnlockAll(boxes):
+    """ Determines if all the boxes can be opened. """
+    count = 0
+    for i in boxes:
+        if i == boxes:
+            count += 1
+            for boxes[i] in boxes:
+                if boxes[i] == boxes:
+                    count += 1
+    return count
