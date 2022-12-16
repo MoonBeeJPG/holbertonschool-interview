c958f86667b4247c03580eaacb0de9df1f27dbaf
Agustina <agustinasoledad2608@gmail.com>
2022-12-15 23:34:05 -0300

    Task 0. Updating files

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -10,3 +10,34 @@
  */
 
  listint_t *insert_node(listint_t **head, int number);
+    listint_t current = *head, listint_t *new_node, listint_t *next_node;
+
+    /* Creating new node */
+    new_node = malloc(sizeof(listint_t));
+    if (!new_node)
+        return (NULL);
+    new_node->n = number;
+    new_node->next = NULL;
+
+    /* First border case: Empty list*/
+    if (!*head)
+    {
+        *head = new_node;
+        return (*head);
+    }
+
+    /* Second border case: Insert it as a first node*/
+    if ((*head)->n > number)
+    {
+        new_node->next = *head;
+        *head = new_node;
+        return (new_node)
+    }
+
+    /* Normal case: Insert it in a part of a list */
+    while (current->next != NULL && current->next->n <= number)
+        current = current->next;
+    next_node = current->next;
+    current->next = new_node;
+    new_node->next = next_node;
+    return (new_node);
